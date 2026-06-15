91d424ed174c92ae9272d340711413893d3a2d3d
KANUMURI KOWSHIK <kanumurikowshik@gmail.com>
2026-06-15 15:52:41 +0530

    sixth commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-//import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Vite config: Tailwind is handled via PostCSS/Tailwind plugin, no special Vite package required
 export default defineConfig({
   plugins: [react()],
-  base: "/inventory_db/", //YourRepoName
+  base: '/inventory_db/'
 })
